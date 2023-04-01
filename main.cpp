f711853af5e747b18e1368759d0f1f8b5feac4f2
Valenthyno <119706519+Valenthyno@users.noreply.github.com>
2023-04-01 10:56:30 -0500

    Create main.cpp

--- main.cpp
+++ main.cpp
@@ -17,7 +17,6 @@ int main()
 	std::cout << "Escuela Profesional de Ciencia de la Computacion I" << std::endl; 
 	std::cout << "Curso de Ciencia de la Computacion I" << std::endl; 
 	std::cout << "Prof. D.Sc. Manuel Eduardo Loaiza Fernandez" << std::endl;
-	std::cout << "Alum. Freddy Diaz Espino" << std::endl; 
 	std::cout << "Alum. Jose Alberto Ordonez Lopez" << std::endl; 
 	std::cout << "Arequipa 2023 - I" << std::endl; 
 	std::cout << "\x1B[3;34m";         // Mostrar el siguiente texto en modo de letra italico "[3;" y color azul "[ ;34m"	
@@ -30,69 +29,65 @@ int main()
 	int turno{1};
 	char a1{'1'},a2{'2'},a3{'3'},b1{'4'},b2{'5'},b3{'6'},c1{'7'},c2{'8'},c3{'9'};
 	char simbolo{'/'};
+	
+	std::cout << "/" << " | " << "/////////////\n";
+	std::cout << "_" << " | " << "1" << " | " << "2" << " | " << "3" << " | " << "\n";
+	std::cout << "a" << " | " << a1 << " | " << a2 << " | " << a3 << " | " << "\n";
+	std::cout << "b" << " | " << b1 << " | " << b2 << " | " << b3 << " | "  << "\n"; 
+	std::cout << "c" << " | " << c1 << " | " << c2 << " | " << c3 << " | " << "\n";
+	std::cout << "/" << " | " << "/////////////\n";
 
-	while (turno <= 9)
-	{
-		std::cout << "/" << " | " << "/////////////\n";
-		std::cout << "_" << " | " << "1" << " | " << "2" << " | " << "3" << " | " << "\n";
-		std::cout << "a" << " | " << a1 << " | " << a2 << " | " << a3 << " | " << "\n";
-		std::cout << "b" << " | " << b1 << " | " << b2 << " | " << b3 << " | "  << "\n"; 
-		std::cout << "c" << " | " << c1 << " | " << c2 << " | " << c3 << " | " << "\n";
-		std::cout << "/" << " | " << "/////////////\n";
+	if ((turno < 9) && (turno >= 0));
 
-		if ((turno < 9) && (turno >= 0));
+		if (turno == 2 && turno == 4 && turno == 6 && turno == 8)
+		{
+			char simbolo{'X'};
+			std::cout << " | " << "TURNO DEL JUGADOR 1" << " | \n";
+		}
 
-			if (turno == 2 && turno == 4 && turno == 6 && turno == 8)
-			{
-				char simbolo{'X'};
-				std::cout << " | " << "TURNO DEL JUGADOR 1" << " | \n";
-			}
+		else
+		{
+			char simbolo{'O'};
+			std::cout << " | " << "TURNO DEL JUGADOR 2" << " | \n";
+		}
 
-			else
-			{
-				char simbolo{'O'};
-				std::cout << " | " << "TURNO DEL JUGADOR 2" << " | \n";
-			}
-
-			switch (1 - 9)
-			{
-			case 1:
-				a1 = simbolo;
-				break;
-			case 2:
-				a2 = simbolo;
-				break;
-			case 3:
-				a3 = simbolo;
-				break;
-			case 4:
-				b1 = simbolo;
-				break;
-			case 5:
-				b2 = simbolo;
-				break;
-			case 6:
-				b3 = simbolo;
-				break;
-			case 7:
-				c1 = simbolo;
-				break;
-			case 8:
-				c2 = simbolo;
-				break;
-			case 9:
-				c3 = simbolo;
-				break;
+		switch (1 - 9)
+		{
+		case 1:
+			a1 = simbolo;
+			break;
+		case 2:
+			a2 = simbolo;
+			break;
+		case 3:
+			a3 = simbolo;
+			break;
+		case 4:
+			b1 = simbolo;
+			break;
+		case 5:
+			b2 = simbolo;
+			break;
+		case 6:
+			b3 = simbolo;
+			break;
+		case 7:
+			c1 = simbolo;
+			break;
+		case 8:
+			c2 = simbolo;
+			break;
+		case 9:
+			c3 = simbolo;
+			break;
 			
-			default:
-				std::cout << "No se ingreso una opcion correcta";
-				break;
-			}
+		default:
+			std::cout << "No se ingreso una opcion correcta";
+			break;
+		}
 
-			turno = turno + 1; 
-			
+		turno = turno + 1; 
 
-	}
 	
 			//if (turno == 1 && turno == 3 && turno == 5 && turno == 7 && turno == 9);
 	
